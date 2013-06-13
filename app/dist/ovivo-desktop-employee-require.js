e9bc0fa1f3153ced48bd93d680ebe2ebe1a0561d
Taras Yevtushenko <taras.evt@gmail.com>
2013-06-13 17:05:24 +0300

    Display groups list on week view

--- app/dist/ovivo-desktop-employee-require.js
+++ app/dist/ovivo-desktop-employee-require.js
@@ -19699,7 +19699,7 @@ function program1(depth0,data) {
   
   return "Loading content";}
 
-  buffer += "<ul class=\"period-groups\">\r\n</ul>\r\n\r\n<div class=\"overlay\">\r\n    <em>\r\n        <span>";
+  buffer += "<ul class=\"period-groups\">\r\n</ul>\r\n\r\n<ul class=\"groups-list\">\r\n</ul>\r\n\r\n<div class=\"overlay\">\r\n    <em>\r\n        <span>";
   foundHelper = helpers.i18n;
   if (foundHelper) { stack1 = foundHelper.call(depth0, {hash:{},inverse:self.noop,fn:self.program(1, program1, data)}); }
   else { stack1 = depth0.i18n; stack1 = typeof stack1 === functionType ? stack1.call(depth0) : stack1; }
@@ -19726,7 +19726,7 @@ function program1(depth0,data) {
   foundHelper = helpers.cid;
   if (foundHelper) { stack1 = foundHelper.call(depth0, {hash:{}}); }
   else { stack1 = depth0.cid; stack1 = typeof stack1 === functionType ? stack1.call(depth0) : stack1; }
-  buffer += escapeExpression(stack1) + "\">\r\n    <ul class=\"period-groups\">\r\n    </ul>\r\n\r\n    <div class=\"overlay\">\r\n        <em>\r\n            <span>";
+  buffer += escapeExpression(stack1) + "\">\r\n    <ul class=\"period-groups\">\r\n    </ul>\r\n\r\n    <ul class=\"groups-list\">\r\n    </ul>\r\n\r\n    <div class=\"overlay\">\r\n        <em>\r\n            <span>";
   foundHelper = helpers.i18n;
   if (foundHelper) { stack1 = foundHelper.call(depth0, {hash:{},inverse:self.noop,fn:self.program(2, program2, data)}); }
   else { stack1 = depth0.i18n; stack1 = typeof stack1 === functionType ? stack1.call(depth0) : stack1; }
