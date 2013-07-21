b40ba24ff1f4a36a0f05f3ad5fdbef242b80046b
Oskar <oskar@localhost.localdomain>
2013-07-21 16:42:03 -0300

    Works

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require("http");
 
 http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
-	    response.write("Welcome to Amazon Beanstalk - NodeJS demo deployment integration");
+	    response.write("Welcome to Amazon Beanstalk - NodeJS demo deployment integration Works!!!");
 	      response.end();
 }).listen(process.env.PORT || 8888);
