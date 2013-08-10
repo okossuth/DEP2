57a756316dd3c457e4c5866a27e0684356a2e909
oskar <oskar@localhost.localdomain>
2013-08-10 10:49:30 -0300

    sdd

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 
-/// beta theta reta dhfjdfhdjf
+/// beta theta reta dhfjdfhdjf ffdfdfdf
 var app, express, getPages, pg, port, processMainPage;
 var proxy, supportProxy;
 var httpProxy = require('http-proxy');
