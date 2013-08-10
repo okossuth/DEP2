13a1752a7952c6ab63789023af39ab64556f396b
oskar <oskar@localhost.localdomain>
2013-08-10 11:18:07 -0300

    gg

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require("http");
 
 http.createServer(function(request, response) {
 	  response.writeHead(200, {"Content-Type": "text/plain"});
-	    response.write("Hello World!! Welcome Sysadmin Oskar!");
+	    response.write("Hello World!! Welcome Sysadmin LOCO!");
 	      response.end();
 }).listen(process.env.PORT || 8888);
 
